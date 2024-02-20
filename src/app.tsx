f2fd2705d72ef9b14af6b2dcb8cb6a9a1328cded
GabrielS0ares <soaresgabriel33@gmail.com>
2024-02-20 13:41:35 -0300

    Feat: instala dependencias, copia componentes do projeto

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,18 @@
-export function App() {
+import { Plus } from "lucide-react";
 
+export function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <div className="py-10 space-y-8">
+      <div>Header tabs</div>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className="flex items-center gap-3">
+          <h1 className="text-xl font-bold">Tags</h1>
+          <button className="inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-2 py-1.5">
+            <Plus className="size-3" />
+            Criar novo
+          </button>
+        </div>
+      </main>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**.*.tsx',
+    './src/**/*.tsx',
     './index.html'
   ],
   theme: {
     extend: {
-      fontFamily : {
+      fontFamily: {
         sans: 'Inter, sans-serif'
       }
     },
   },
   plugins: [],
 }
-
