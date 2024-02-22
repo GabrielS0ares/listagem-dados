d9450adfd368981283114e1dc52a87a8d87ace46
gabrielS0ares <soaresgabriel33@gmail.com>
2024-02-22 00:02:37 -0300

    Feat: configura consuilta por meio da alteracao da rota usando cache

--- src/app.tsx
+++ src/app.tsx
@@ -11,8 +11,9 @@ import {
   TableHeader,
   TableRow,
 } from "./components/ui/table";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { Pagination } from "./components/pagination";
+import { useSearchParams } from "react-router-dom";
 
 export interface TagResponse {
   first: number;
@@ -32,17 +33,22 @@ export interface Tag {
 }
 
 export function App() {
+  const [searchParams] = useSearchParams();
+
+  const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
+
   const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
     queryFn: async () => {
       const response = await fetch(
-        "http://localhost:3333/tags?_page=1&_per_page=10"
+        `http://localhost:3333/tags?_page=${page}&_per_page=10`
       );
       const data = await response.json();
       console.log("data", data);
 
       return data;
     },
-    queryKey: ["get-tags"],
+    placeholderData: keepPreviousData,
+    queryKey: ["get-tags", page],
   });
 
   if (isLoading) {
@@ -114,7 +120,7 @@ export function App() {
           <Pagination
             pages={tagsResponse.pages}
             items={tagsResponse.items}
-            page={1}
+            page={page}
           />
         )}
       </main>
