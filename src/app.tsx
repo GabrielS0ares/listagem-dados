b9aa2c7a18fb0f03cd0c3ca3b98d0165677df514
gabrielS0ares <soaresgabriel33@gmail.com>
2024-02-21 23:47:30 -0300

    Feat: configura multplas rotas para o funcionamento do search params

--- src/app.tsx
+++ src/app.tsx
@@ -12,41 +12,41 @@ import {
   TableRow,
 } from "./components/ui/table";
 import { useQuery } from "@tanstack/react-query";
-// import { Pagination } from "./components/pagination";
+import { Pagination } from "./components/pagination";
 
 export interface TagResponse {
-  first: number
-  prev: number | null
-  next: number
-  last: number
-  pages: number
-  items: number
-  data: Tag[]
+  first: number;
+  prev: number | null;
+  next: number;
+  last: number;
+  pages: number;
+  items: number;
+  data: Tag[];
 }
 
 export interface Tag {
-  title: string
-  slug: string
-  amountOfVideos: number
-  id: string
+  title: string;
+  slug: string;
+  amountOfVideos: number;
+  id: string;
 }
 
-
 export function App() {
-
-  const { data : tagsResponse, isLoading } = useQuery<TagResponse>({
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
     queryFn: async () => {
-      const response = await fetch('http://localhost:3333/tags?_page=1&_per_page=10')
-      const data = await  response.json()
-      console.log("data", data)
+      const response = await fetch(
+        "http://localhost:3333/tags?_page=1&_per_page=10"
+      );
+      const data = await response.json();
+      console.log("data", data);
 
-      return data
+      return data;
     },
-    queryKey: ['get-tags']
-  })
+    queryKey: ["get-tags"],
+  });
 
-  if(isLoading){
-    null
+  if (isLoading) {
+    null;
   }
 
   return (
@@ -94,12 +94,12 @@ export function App() {
                   <TableCell>
                     <div className="flex flex-col">
                       <span className="font-medium ">{tag.title}</span>
-                      <span className="text-xs text-zinc-500">
-                        {tag.id}
-                      </span>
+                      <span className="text-xs text-zinc-500">{tag.id}</span>
                     </div>
                   </TableCell>
-                  <TableCell className="text-zinc-500">{tag.amountOfVideos} video(s)</TableCell>
+                  <TableCell className="text-zinc-500">
+                    {tag.amountOfVideos} video(s)
+                  </TableCell>
                   <TableCell className="text-right">
                     <Button size="icon">
                       <MoreHorizontal size={3} />
@@ -110,8 +110,13 @@ export function App() {
             })}
           </TableBody>
         </Table>
-
-        {/* <Pagination /> */}
+        {tagsResponse && (
+          <Pagination
+            pages={tagsResponse.pages}
+            items={tagsResponse.items}
+            page={1}
+          />
+        )}
       </main>
     </div>
   );
